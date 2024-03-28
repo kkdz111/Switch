fe4a3258cc26528d1e303c345be1ad3735ee18c0
SunChengze <77314919+kkdz111@users.noreply.github.com>
2024-03-28 18:29:59 +0800

    更新 123.java

--- 123.java
+++ 123.java
@@ -21,6 +21,16 @@ Scanner sc = new Scanner(System.in);
   int a = sc.nextInt();
   System.out.println("请输入第二个整数:");
   int b = sc.nextInt();
-  System.out.println("请输入您要进行的运算(1:表示加法,2:表示减法,3:表示乘法,4:表示除法)");    
+  System.out.println("请输入您要进行的运算(1:表示加法,2:表示减法,3:表示乘法,4:表示除法)"); 
+  int dayOfWeek = 2;
+switch (dayOfWeek) {
+    case 1:
+      System.out.println(a+b);
+    case 2:
+      System.out.println(a-b);
+    case 3:
+      System.out.println(a*b);
+    case 4:
+      System.out.println(a/b);   
   }
 }
